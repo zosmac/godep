8741b7cb9b383757dcc97e8c1566cc0adef19493
zosmac <zosmac@github.com>
2023-01-22 18:01:17 -0500

    rename repo to godep

--- doc.go
+++ doc.go
@@ -1,8 +1,8 @@
 package main
 
 /*
-Package gonode defines the gonode command that examines the source files of a Go language
-module. It produces a node graph of the relationships of the module's packages with packages
-of the Go standard library, of imports, and of those that are vendored. Gonode uses the
+Package godep defines the godep command that examines the source files of a Go language
+module. It produces a node graph of the dependencies of the module's packages on packages
+of the Go standard library, of imports, and of those that are vendored. Godep uses the
 Graphvis dot command to produce the nodegraph.
 */
--- go.mod
+++ go.mod
@@ -1,15 +1,15 @@
-module github.com/zosmac/gonode
+module github.com/zosma/godep
 
 go 1.19
 
 require (
-	github.com/zosmac/gomon v0.0.0-20221213021833-0b0ecea02a23
+	github.com/zosmac/gomon v0.0.0-20230122223040-b03a89075161
 	golang.org/x/tools v0.5.0
 )
 
 require (
 	github.com/StackExchange/wmi v1.2.1 // indirect
-	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	golang.org/x/mod v0.7.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
